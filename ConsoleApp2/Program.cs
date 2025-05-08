2e14c4fbcfc6afb3a47f93bbd2a7152c41b0d5ae
sibel <sibel@SIBEL_PC>
2025-05-08 16:37:04 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sibel Doğru Kariyerime Patika'nın uzman ve deneyimli kadrosuyla yazılım dünyasında devam etmek için buradayım :)");
+
+
+        }
+
+    }
+}
